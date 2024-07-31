2b871c90c4b3e24cfcd8055c06e6f942cf6b00cf
Mehmet Anıl <m.cmert1907@hotmail.com>
2024-07-31 21:32:06 +0300

    Envanter güncellemesi
    Envanterdeki renklendirme UI kasma sorunu haledildi

--- wealth_calculator/lib/inventory.dart
+++ wealth_calculator/lib/inventory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:wealth_calculator/inventory/ItemDialogs.dart';
 import 'package:wealth_calculator/inventory/TotalPrice.dart';
 import 'package:wealth_calculator/inventory/WealthList.dart';
@@ -19,11 +20,11 @@ class InventoryScreen extends StatefulWidget {
 class _InventoryScreenState extends State<InventoryScreen> {
   late Future<List<SavedWealths>> futureSavedWealths;
   List<SavedWealths> savedWealths = [];
-  late Map<SavedWealths, int> selectedItems = {};
-  double totalPrice = 0; // Global totalPrice değişkeni
-  List<double> segments = []; // Global segments değişkeni
-  List<Color> colors = []; // Global colors değişkeni
-
+  List<WealthPrice> goldPrices = [];
+  List<WealthPrice> currencyPrices = [];
+  double totalPrice = 0;
+  List<double> segments = [];
+  List<Color> colors = [];
   final Map<String, Color> colorMap = {
     'Altın (TL/GR)': Colors.yellow,
     'ABD Doları': Colors.green,
@@ -35,82 +36,37 @@ class _InventoryScreenState extends State<InventoryScreen> {
   @override
   void initState() {
     super.initState();
-    _loadWealths();
+    _loadData();
   }
 
-  Future<void> _loadWealths() async {
+  Future<void> _loadData() async {
     try {
-      savedWealths = await SavedWealthsdao().getAllWealths();
-      await _calculateTotalPrice(); // Veriler yüklendiğinde toplam fiyatı hesapla
+      // Fetch saved wealths and prices concurrently
+      final results = await Future.wait([
+        SavedWealthsdao().getAllWealths(),
+        widget.futureGoldPrices,
+        widget.futureCurrencyPrices,
+      ]);
+
+      setState(() {
+        savedWealths = results[0] as List<SavedWealths>;
+        goldPrices = results[1] as List<WealthPrice>;
+        currencyPrices = results[2] as List<WealthPrice>;
+        _calculateTotalPrice(); // Calculate total price and segments based on new data
+      });
     } catch (e) {
+      // Handle error
       setState(() {
         savedWealths = [];
-        totalPrice = 0; // Hata durumunda toplam fiyatı sıfırlıyoruz
+        goldPrices = [];
+        currencyPrices = [];
+        totalPrice = 0;
       });
     }
   }
 
-  Future<void> _refreshWealths() async {
-    try {
-      savedWealths = await SavedWealthsdao().getAllWealths();
-      await _calculateTotalPrice(); // Varlıklar güncellendikten sonra toplam fiyatı hesapla
-    } catch (e) {
-      setState(() {
-        savedWealths = [];
-        totalPrice = 0; // Hata durumunda toplam fiyatı sıfırlıyoruz
-      });
-    }
-  }
-
-  void _deleteWealth(int id) async {
-    await SavedWealthsdao().deleteWealth(id);
-    setState(() {
-      savedWealths.removeWhere((wealth) => wealth.id == id);
-      _calculateTotalPrice(); // Varlık silindikten sonra toplam fiyatı ve segmentleri hesapla
-    });
-  }
-
-  void _editWealth(SavedWealths wealth, int amount) async {
-    SavedWealthsdao wealthsDao = SavedWealthsdao();
-    SavedWealths? existingWealth =
-        await wealthsDao.getWealthByType(wealth.type);
-
-    if (existingWealth != null) {
-      SavedWealths updatedWealth = SavedWealths(
-        id: existingWealth.id,
-        type: existingWealth.type,
-        amount: amount,
-      );
-      await wealthsDao.updateWealth(updatedWealth);
-      setState(() {
-        int index = savedWealths.indexWhere((w) => w.id == updatedWealth.id);
-        if (index != -1) {
-          savedWealths[index] = updatedWealth;
-        }
-        _calculateTotalPrice(); // Varlık güncellendikten sonra toplam fiyatı ve segmentleri hesapla
-      });
-    } else {
-      SavedWealths newWealth = SavedWealths(
-        id: DateTime.now().millisecondsSinceEpoch,
-        type: wealth.type,
-        amount: amount,
-      );
-      await wealthsDao.insertWealth(newWealth);
-      setState(() {
-        savedWealths.add(newWealth);
-        _calculateTotalPrice(); // Yeni varlık eklendikten sonra toplam fiyatı ve segmentleri hesapla
-      });
-    }
-  }
-
-  Future<void> _calculateTotalPrice() async {
-    List<List<WealthPrice>> prices = await Future.wait(
-        [widget.futureGoldPrices, widget.futureCurrencyPrices]);
-    List<WealthPrice> goldPrices = prices[0];
-    List<WealthPrice> currencyPrices = prices[1];
-
+  void _calculateTotalPrice() {
     double total = 0;
-
     for (var wealth in savedWealths) {
       double price = 0.0;
       for (var gold in goldPrices) {
@@ -130,25 +86,20 @@ class _InventoryScreenState extends State<InventoryScreen> {
       }
       total += price * wealth.amount;
     }
-
     setState(() {
       totalPrice = total;
     });
 
-    // Segment hesaplama ve güncelleme
-    List<double> newSegments = calculateSegments(
+    // Calculate segments
+    segments = calculateSegments(
       {for (var wealth in savedWealths) wealth: wealth.amount},
       goldPrices,
       currencyPrices,
       colorMap,
     );
-
-    setState(() {
-      segments = newSegments;
-      colors = [
-        for (var wealth in savedWealths) colorMap[wealth.type] ?? Colors.grey
-      ];
-    });
+    colors = [
+      for (var wealth in savedWealths) colorMap[wealth.type] ?? Colors.grey
+    ];
   }
 
   List<double> calculateSegments(
@@ -157,9 +108,7 @@ class _InventoryScreenState extends State<InventoryScreen> {
       List<WealthPrice> currencyPrices,
       Map<String, Color> colorMap) {
     List<double> segments = [];
-    List<Color> colors = [];
     double total = 0;
-
     for (var entry in selectedItems.entries) {
       double price = 0.0;
       for (var gold in goldPrices) {
@@ -180,30 +129,52 @@ class _InventoryScreenState extends State<InventoryScreen> {
       double value = price * entry.value;
       total += value;
       segments.add(value);
-      colors.add(colorMap[entry.key.type] ?? Colors.grey);
     }
 
-    // Normalize the segments
     if (total > 0) {
       segments = segments.map((segment) => (segment / total) * 360).toList();
     }
-
-    // Grouping gray segments at the end
-    List<double> graySegments = [];
-    List<double> nonGraySegments = [];
-    for (int i = 0; i < segments.length; i++) {
-      if (colors[i] == Colors.grey) {
-        graySegments.add(segments[i]);
-      } else {
-        nonGraySegments.add(segments[i]);
-      }
-    }
-
-    segments = nonGraySegments + graySegments;
-    colors = colors.where((color) => color != Colors.grey).toList()
-      ..addAll(List.filled(graySegments.length, Colors.grey));
-
     return segments;
+  }
+
+  void _deleteWealth(int id) async {
+    await SavedWealthsdao().deleteWealth(id);
+    setState(() {
+      savedWealths.removeWhere((wealth) => wealth.id == id);
+      _calculateTotalPrice(); // Recalculate after deletion
+    });
+  }
+
+  void _editWealth(SavedWealths wealth, int amount) async {
+    SavedWealthsdao wealthsDao = SavedWealthsdao();
+    SavedWealths? existingWealth =
+        await wealthsDao.getWealthByType(wealth.type);
+    if (existingWealth != null) {
+      SavedWealths updatedWealth = SavedWealths(
+        id: existingWealth.id,
+        type: existingWealth.type,
+        amount: amount,
+      );
+      await wealthsDao.updateWealth(updatedWealth);
+      setState(() {
+        int index = savedWealths.indexWhere((w) => w.id == updatedWealth.id);
+        if (index != -1) {
+          savedWealths[index] = updatedWealth;
+        }
+        _calculateTotalPrice(); // Recalculate after update
+      });
+    } else {
+      SavedWealths newWealth = SavedWealths(
+        id: DateTime.now().millisecondsSinceEpoch,
+        type: wealth.type,
+        amount: amount,
+      );
+      await wealthsDao.insertWealth(newWealth);
+      setState(() {
+        savedWealths.add(newWealth);
+        _calculateTotalPrice(); // Recalculate after adding new wealth
+      });
+    }
   }
 
   @override
@@ -211,46 +182,31 @@ class _InventoryScreenState extends State<InventoryScreen> {
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            'Varlık Hesaplayıcı',
+            'Varlık Hesaplama Makinesi',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.blueGrey,
         ),
-        body: Column(
-          children: [
-            Container(
-                child: Stack(
-              children: [
-                Positioned(
-                  child: FutureBuilder<List<List<WealthPrice>>>(
-                    future: Future.wait(
-                      [widget.futureGoldPrices, widget.futureCurrencyPrices],
-                    ),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return CircularProgressIndicator();
-                      } else if (snapshot.hasError) {
-                        return Text('Hata: ${snapshot.error}');
-                      } else if (snapshot.hasData) {
-                        List<WealthPrice> goldPrices = snapshot.data![0];
-                        List<WealthPrice> currencyPrices = snapshot.data![1];
-
-                        return TotalPrice(
-                          totalPrice: totalPrice,
-                          segments: segments,
-                          colors: colors,
-                        );
-                      } else {
-                        return Text('Veri bulunamadı');
-                      }
-                    },
-                  ),
+        body: CustomScrollView(
+          slivers: [
+            // SliverAppBar for TotalPrice
+            SliverAppBar(
+              expandedHeight: 200.0, // Adjust height as needed
+              collapsedHeight: 250,
+              automaticallyImplyLeading: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: TotalPrice(
+                  totalPrice: totalPrice,
+                  segments: segments,
+                  colors: colors,
                 ),
-                Positioned(
-                  right: 14,
-                  top: 0, // FloatingActionButton'un konumu
+              ),
+              actions: [
+                // Adding FloatingActionButton to AppBar's actions
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0, top: 2),
                   child: FloatingActionButton(
-                    backgroundColor: const Color.fromARGB(255, 56, 160, 246),
+                    backgroundColor: Color.fromARGB(255, 59, 150, 223),
                     onPressed: () {
                       ItemDialogs.showSelectItemDialog(
                         context,
@@ -278,27 +234,56 @@ class _InventoryScreenState extends State<InventoryScreen> {
                   ),
                 ),
               ],
-            )),
-            Expanded(
-              child: savedWealths.isEmpty
-                  ? Center(
-                      child: Text(
-                      'Varlık Bulunamadı',
-                      style: TextStyle(fontSize: 25),
-                    ))
-                  : WealthList(
-                      selectedItems: {
-                        for (var wealth in savedWealths) wealth: wealth.amount,
-                      },
-                      onDelete: _deleteWealth,
-                      onEdit: (entry) {
+            ),
+            // SliverList for WealthList
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  if (savedWealths.isEmpty) {
+                    return Center(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 150),
+                        child: Text(
+                          'Varlık Bulunamadı',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                    );
+                  } else {
+                    final wealth = savedWealths[index];
+                    return ListTile(
+                      title: Text('${wealth.type}'),
+                      titleTextStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21),
+                      subtitle: Text('Miktar: ${wealth.amount}'),
+                      subtitleTextStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                      tileColor: Colors.blueGrey,
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          color: Color.fromARGB(255, 235, 235, 235),
+                        ),
+                        onPressed: () {
+                          _deleteWealth(wealth.id);
+                        },
+                      ),
+                      onTap: () {
                         ItemDialogs.showEditItemDialog(
                           context,
-                          entry,
+                          MapEntry(wealth, wealth.amount),
                           _editWealth,
                         );
                       },
-                    ),
+                    );
+                  }
+                },
+                childCount: savedWealths.isEmpty ? 1 : savedWealths.length,
+              ),
             ),
           ],
         ));
