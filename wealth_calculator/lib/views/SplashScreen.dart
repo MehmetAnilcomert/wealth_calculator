e13d0364ec8ea0a1f5a16eb7f7fc17e150c8dce4
Mehmet Anıl <m.cmert1907@hotmail.com>
2024-08-05 21:47:11 +0300

    Animasyon sırasında ana ekran yükleniyor.
    Yeni özellik ile kullanıcı deneyimi arttırıldı.

--- wealth_calculator/lib/views/SplashScreen.dart
+++ wealth_calculator/lib/views/SplashScreen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
+import 'package:wealth_calculator/bloc/Bloc/PricesBloc/PricesEvent.dart';
+import 'package:wealth_calculator/bloc/Bloc/PricesBloc/PricesState.dart';
+import 'package:wealth_calculator/bloc/Bloc/PricesBloc/pricesBloc.dart';
 import 'package:wealth_calculator/views/PriceViews.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,37 +15,68 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
+  bool _loadingComplete = false;
+
   @override
   void initState() {
     super.initState();
     startTimer();
+    _startLoadingData();
   }
 
-  startTimer() {
-    Duration duration = Duration(seconds: 3);
-    return Timer(duration, goHome);
+  void startTimer() {
+    Duration duration = Duration(seconds: 3); // Animasyon süresi
+    _timer = Timer(duration, _checkLoadingState);
   }
 
-  void goHome() {
+  void _startLoadingData() {
+    // PricesScreen için gerekli verilerin yüklenmesini başlatın
+    context.read<GoldPricesBloc>().add(LoadGoldPrices());
+  }
+
+  void _checkLoadingState() {
+    if (_loadingComplete) {
+      _goHome();
+    } else {
+      // Eğer yükleme tamamlanmadıysa dinlemeye devam et
+      _timer = Timer(Duration(milliseconds: 500), _checkLoadingState);
+    }
+  }
+
+  void _goHome() {
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
       return PricesScreen();
     }));
   }
 
   @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: SizedBox(
-            width: MediaQuery.sizeOf(context).width * 0.7,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset("images/logo.json"),
-                Lottie.asset("images/isim.json")
-              ],
-            )),
+    return BlocListener<GoldPricesBloc, GoldPricesState>(
+      listener: (context, state) {
+        if (state is GoldPricesLoaded) {
+          _loadingComplete = true;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: SizedBox(
+              width: MediaQuery.sizeOf(context).width * 0.7,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset("images/logo.json"),
+                  Lottie.asset("images/isim.json")
+                ],
+              )),
+        ),
       ),
     );
   }
