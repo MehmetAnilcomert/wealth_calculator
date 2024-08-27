3ef65502792fbb6efa8faeb8260236fe188f5ec1
Mehmet Anıl <m.cmert1907@hotmail.com>
2024-08-27 19:32:03 +0300

    Envanter güncellendi
    Envanterin ana ekranda iken yüklenmiş olması sağlandı

--- wealth_calculator/lib/views/inventory_screen.dart
+++ wealth_calculator/lib/views/inventory_screen.dart
@@ -11,80 +11,81 @@ import 'package:wealth_calculator/widgets/InventoryWidgets/list_widget.dart';
 class InventoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => InventoryBloc()..add(LoadInventoryData()),
-      child: Scaffold(
+    // Mevcut Bloc'u kullanmak için BlocProvider.value kullanıyoruz
+    final inventoryBloc = BlocProvider.of<InventoryBloc>(context);
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Varlıklar',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            'Varlıklar',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: BlocConsumer<InventoryBloc, InventoryState>(
-          listener: (context, state) {
-            if (state is InventoryError) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Hata: Veriler yüklenemedi'),
-                  backgroundColor: Colors.red,
+      ),
+      body: BlocConsumer<InventoryBloc, InventoryState>(
+        bloc: inventoryBloc, // Aynı Bloc'u kullandığınızdan emin olun
+        listener: (context, state) {
+          if (state is InventoryError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Hata: Veriler yüklenemedi'),
+                backgroundColor: Colors.red,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is InventoryLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is InventoryLoaded) {
+            return CustomScrollView(
+              slivers: [
+                CustomSliverAppBar(
+                  expandedHeight: MediaQuery.of(context).size.height * 0.35,
+                  collapsedHeight: MediaQuery.of(context).size.height * 0.35,
+                  flexibleSpaceBackground: TotalPrice(
+                    totalPrice: state.totalPrice,
+                    segments: state.segments,
+                    colors: state.colors,
+                  ),
+                  onAddPressed: () {
+                    ItemDialogs.showSelectItemDialog(
+                      context,
+                      state.goldPrices,
+                      state.currencyPrices,
+                      (wealth, amount) {
+                        ItemDialogs.showEditItemDialog(
+                          context,
+                          MapEntry(wealth, amount),
+                          (wealth, amount) {
+                            context
+                                .read<InventoryBloc>()
+                                .add(AddOrUpdateWealth(wealth, amount));
+                          },
+                        );
+                      },
+                      hiddenItems: [
+                        'Altın (ONS/\$)',
+                        'Altın (\$/kg)',
+                        'Altın (Euro/kg)',
+                        'Külçe Altın (\$)'
+                      ],
+                    );
+                  },
+                  bloc: inventoryBloc, // Aynı Bloc'u kullanın
                 ),
-              );
-            }
-          },
-          builder: (context, state) {
-            if (state is InventoryLoading) {
-              return Center(child: CircularProgressIndicator());
-            } else if (state is InventoryLoaded) {
-              return CustomScrollView(
-                slivers: [
-                  CustomSliverAppBar(
-                    expandedHeight: MediaQuery.of(context).size.height * 0.35,
-                    collapsedHeight: MediaQuery.of(context).size.height * 0.35,
-                    flexibleSpaceBackground: TotalPrice(
-                      totalPrice: state.totalPrice,
-                      segments: state.segments,
-                      colors: state.colors,
-                    ),
-                    onAddPressed: () {
-                      ItemDialogs.showSelectItemDialog(
-                        context,
-                        state.goldPrices,
-                        state.currencyPrices,
-                        (wealth, amount) {
-                          ItemDialogs.showEditItemDialog(
-                            context,
-                            MapEntry(wealth, amount),
-                            (wealth, amount) {
-                              context
-                                  .read<InventoryBloc>()
-                                  .add(AddOrUpdateWealth(wealth, amount));
-                            },
-                          );
-                        },
-                        hiddenItems: [
-                          'Altın (ONS/\$)',
-                          'Altın (\$/kg)',
-                          'Altın (Euro/kg)',
-                          'Külçe Altın (\$)'
-                        ],
-                      );
-                    },
-                    bloc: context.read<InventoryBloc>(),
-                  ),
-                  SliverFillRemaining(
-                    child: InventoryListWidget(
-                        colors: state.colors, savedWealths: state.savedWealths),
-                  ),
-                ],
-              );
-            } else {
-              return Center(child: Text('Veriler yüklenirken hata oldu'));
-            }
-          },
-        ),
+                SliverFillRemaining(
+                  child: InventoryListWidget(
+                      colors: state.colors, savedWealths: state.savedWealths),
+                ),
+              ],
+            );
+          } else {
+            return Center(child: Text('Veriler yüklenirken hata oldu'));
+          }
+        },
       ),
     );
   }
