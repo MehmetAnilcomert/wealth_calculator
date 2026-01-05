91fcf916111ec4a1776096324c379cef52b4ad49
Mehmet Anıl <m.cmert1907@hotmail.com>
2026-01-06 00:34:50 +0300

    compile error fixed

--- wealth_calculator/lib/feature/settings/settings.dart
+++ wealth_calculator/lib/feature/settings/settings.dart
@@ -1,5 +1,2 @@
-export 'viewmodel/temp_calc_event.dart';
-export 'viewmodel/temp_calc_state.dart';
 export 'view/settings_view.dart';
 export 'view/profile_view.dart';
-export 'view/temp_calculator_view.dart';
