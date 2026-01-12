a2b21c4c86ca90e245c0471b436868d8af4d9d16
Mehmet Anıl <m.cmert1907@hotmail.com>
2026-01-12 17:50:15 +0300

    some refactorings

--- wealth_calculator/module/core/lib/src/cache/core/cache_operation.dart
+++ wealth_calculator/module/core/lib/src/cache/core/cache_operation.dart
@@ -1,12 +1,25 @@
 import 'package:core/src/cache/core/cache_model.dart';
 
+/// An abstract class defining cache operations for a generic cache model type [T].
 abstract class CacheOperation<T extends CacheModel> {
+  /// Adds an item to the cache.
   void add(T item);
+
+  /// Adds multiple items to the cache.
   void remove(String id);
+
+  /// Removes multiple items from the cache by their IDs.
   void removeAll(List<String> ids);
+
+  /// Adds multiple items to the cache.
   void addAll(List<T> items);
+
+  /// Clears all items from the cache.
   void clear();
 
+  /// Retrieves an item from the cache by its ID.
   T? get(String id);
+
+  /// Retrieves all items from the cache.
   List<T> getAll();
 }
